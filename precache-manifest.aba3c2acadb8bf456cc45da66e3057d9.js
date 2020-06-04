ea51712b6fffe8a575be01fe16b765e43929ce5b
DosImpact <ypd03008@gmail.com>
2020-06-04 19:32:19 +0900

    Updates

---
+++ precache-manifest.aba3c2acadb8bf456cc45da66e3057d9.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f331e5d09d7273a20150cb92ca95471",
+    "url": "/blockcrawl_landpage/index.html"
+  },
+  {
+    "revision": "bde427e6de6526cb477c",
+    "url": "/blockcrawl_landpage/static/css/main.e5a10588.chunk.css"
+  },
+  {
+    "revision": "3a9b7ba6d0379e126acd",
+    "url": "/blockcrawl_landpage/static/js/2.7368e40e.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/blockcrawl_landpage/static/js/2.7368e40e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bde427e6de6526cb477c",
+    "url": "/blockcrawl_landpage/static/js/main.50fb50bf.chunk.js"
+  },
+  {
+    "revision": "e3c7ba5a0c0080925136",
+    "url": "/blockcrawl_landpage/static/js/runtime-main.63be4016.js"
+  },
+  {
+    "revision": "74fe584c6fda556abf362d3565aea18f",
+    "url": "/blockcrawl_landpage/static/media/001-plaster.74fe584c.svg"
+  },
+  {
+    "revision": "e7605998ec1722926d08444e159ad68a",
+    "url": "/blockcrawl_landpage/static/media/002-medicine.e7605998.svg"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/blockcrawl_landpage/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "411dd3a7e4d90af769fece559127712e",
+    "url": "/blockcrawl_landpage/static/media/cutoIcon.411dd3a7.svg"
+  },
+  {
+    "revision": "6a1776963deb39d710bae80bb449aebe",
+    "url": "/blockcrawl_landpage/static/media/feature-tile-icon-06.6a177696.svg"
+  },
+  {
+    "revision": "d9cb99ceb6a66250792ed78f5765325b",
+    "url": "/blockcrawl_landpage/static/media/features-split-image-01.d9cb99ce.png"
+  },
+  {
+    "revision": "3c569239f88160018f79029ba5dc227d",
+    "url": "/blockcrawl_landpage/static/media/features-split-image-02.3c569239.png"
+  },
+  {
+    "revision": "87e4d053756c60c9f421195d8eafdbfe",
+    "url": "/blockcrawl_landpage/static/media/features-split-image-03.87e4d053.png"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/blockcrawl_landpage/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/blockcrawl_landpage/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "a622fc5dd8adcdd27ea42218385013b4",
+    "url": "/blockcrawl_landpage/static/media/video-placeholder.a622fc5d.jpg"
+  }
+]);
